86c674858d8aba108bf817d52c43083c01480d08
LordWatson <alexander.watson.work@gmail.com>
2022-09-13 09:42:31 +0100

    github actions test without vite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    // Issue with Github Actions where Vite manifest is not found causing tests to fail
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite(); //Fixes it :D
+    }
 }
